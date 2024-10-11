de8d4002ac4b2fa8e6d2f3ecf3db777ac6c45f49
Pandaa Xi <127378505+pandaaxi@users.noreply.github.com>
2024-10-11 15:32:47 +0800

    Update realm.sh

--- realm.sh
+++ realm.sh
@@ -38,12 +38,6 @@ download_realm() {
     mv realm /usr/local/bin/
 }
 
-# Create a symbolic link for the script to make it callable as 'realm'
-create_symlink() {
-    ln -sf $(realpath $0) /usr/local/bin/realm_manager
-    chmod +x /usr/local/bin/realm_manager
-    export PATH=/usr/local/bin:$PATH
-}
 
 # Check if realm is installed
 if [ -f "/usr/local/bin/realm" ]; then
@@ -127,9 +121,7 @@ use_udp = true
         echo "[network] 配置已存在，跳过添加。"
     fi
 
-    # Create symlink for the script
-    create_symlink
-
+    
     # Update realm status
     realm_status="已安装"
     realm_status_color="\033[0;32m" # 绿色
@@ -144,8 +136,7 @@ uninstall_realm() {
     systemctl daemon-reload
     rm -rf /root/realm
     rm -f /usr/local/bin/realm
-    rm -f /usr/local/bin/realm_manager
-    sed -i '/realm/d' /etc/crontab
+        sed -i '/realm/d' /etc/crontab
     echo "realm已被卸载。"
     # Update realm status
     realm_status="未安装"
